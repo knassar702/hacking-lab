07aaeeab4cf51b4fdfc78c32ca06c5baef756b1f
knassar702 <root@localhost.localdomain>
2019-09-07 00:53:39 +0200

    Blog

--- README.md
+++ README.md
@@ -1,39 +1,6 @@
-# [Start Bootstrap](http://startbootstrap.com/) - [Clean Blog](http://startbootstrap.com/template-overviews/clean-blog/)
+# Blog
 
-[Clean Blog](http://startbootstrap.com/template-overviews/clean-blog/) is a stylish, responsive blog theme for [Bootstrap](http://getbootstrap.com/) created by [Start Bootstrap](http://startbootstrap.com/). This theme features a blog homepage, about page, contact page, and an example post page along with a working PHP contact form.
-
-## Getting Started
-
-To use this theme, choose one of the following options to get started:
-* Download the latest release on Start Bootstrap
-* Fork this repository on GitHub
-
-## Flask Usage:
-
-- Install python 2.7
-- Install python-pip
-- Install flask: `pip install flask`
-- Clone this repo
-
-```sh
-git clone https://github.com/ducnc/blog-flask.git
-cd blog-flask
-```
-
-First time see this web app? please run the following commands to init database:
-
-```sh
-python
-$ from flaskr import init_db
-$ init_db()
-```
-
-Now you are ready to go:
-
-$ python flaskr.py
-
-Go to http://127.0.0.1:5000/login with admin/default and enjoin it
-
-## Copyright and License
-
-Copyright 2013-2015 Iron Summit Media Strategies, LLC. Code released under the [Apache 2.0](https://github.com/IronSummitMedia/startbootstrap-clean-blog/blob/gh-pages/LICENSE) license.
+* Flask
+* Jinja
+* Sqlite
+* Bootstrap
--- flaskr.py
+++ flaskr.py
@@ -1,9 +1,9 @@
-__author__ = 'Duc'
+__author__ = 'Khaled Nassar'
 
 # all the imports
-import sqlite3
+import sqlite3,time,datetime
 from flask import Flask, request, session, g, redirect, url_for, \
-        abort, render_template, flash
+        abort, render_template, flash,make_response
 from contextlib import closing
 
 # configuration
@@ -11,8 +11,10 @@ DATABASE = 'flaskr.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
-PASSWORD = 'default'
-
+PASSWORD = 'admin'
+name = 'Hacking-Blog'
+now = datetime.datetime.today()
+date = str(now.year)+'-'+str(now.month)+'-'+str(now.day)
 # application
 
 app = Flask(__name__)
@@ -41,31 +43,70 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
-
+@app.errorhandler(404) 
+# inbuilt function which takes error as parameter 
+def not_found(e): 
+# defining function 
+  return render_template("404.html") 
 @app.route('/')
 @app.route('/index')
+@app.route('/index.html')
+@app.route('/home')
+@app.route('/home.html')
 def index():
-    cur = g.db.execute('select id, title, description from entries order by id desc')
+    cur = g.db.execute('select id, title, description, date from entries order by id desc limit 8')
     a = cur.fetchall()
-    entries = [dict(id=row[0],title=row[1], description=row[2]) for row in a]
+    entries = [dict(id=row[0],title=row[1], description=row[2],d=row[3]) for row in a]
     print entries
-    return render_template('index.html',entries=entries)
-
-
+    response=make_response(render_template('index.html',entries=entries,Name=name))
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['X-XSS-Protection'] = '1; mode=block'
+    return response
+@app.route('/posts')
+def posts():
+    cur = g.db.execute('select id, title, description, date from entries order by id desc')
+    a = cur.fetchall()
+    entries = [dict(id=row[0],title=row[1], description=row[2],d=row[3]) for row in a]
+    return render_template('index.html',entries=entries,Name=name)
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entries (title, description, text) values (?, ?, ?)',
-                 [request.form['title'],request.form['description'], request.form['text']])
+    g.db.execute('insert into entries (title, description, text, date) values (?, ?, ?, ?)',
+                 [request.form['title'],request.form['description'], request.form['text'],str(date)])
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('index'))
-
-
+@app.route('/login/ok/<int:id>',methods=['POST'])
+def ok(id):
+    if session['logged_in'] == True:
+        g.db.execute("UPDATE entries SET title = ?,description = ?,text=? WHERE id= ? ",[request.form['title'],request.form['description'],request.form['text'],id])
+        g.db.commit()
+        return redirect(url_for('index'))    
+@app.route('/login/edite/<int:id>')
+def edite(id):
+    error= None
+    if session['logged_in'] == True:
+        cur = g.db.execute('select id, title, description, date, text from entries where id = {}'.format(id))
+        a = cur.fetchall()
+        id=str(id)
+        entries = [dict(id=row[0],title=row[1], description=row[2],text=row[4]) for row in a]
+        return render_template('edite.html',Name=name,entries=entries)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    cur = g.db.execute('select id, title, description, date from entries order by id desc')
+    a = cur.fetchall()
+    entries = [dict(id=row[0],title=row[1], description=row[2],d=row[3]) for row in a]
     error = None
+    response=make_response(render_template('login.html',error=error))
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['X-XSS-Protection'] = '1; mode=block'
+    response.set_cookie('username', 'flask', secure=True, httponly=True)
+    response.set_cookie('snakes', '3', max_age=600)
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
             error = 'Invalid username'
@@ -74,31 +115,23 @@ def login():
         else:
             session['logged_in'] = True
             flash('You were logged in')
-            return render_template('show_entries.html', error=error)
-    return render_template('login.html', error=error)
+            return render_template('show_entries.html', error=error,entries=entries)
+    return response
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
-
-
-@app.route('/posts/')
-def posts():
-    cur = g.db.execute("select id, title, description, text from entries")
-    a = cur.fetchall()
-    entries = [dict(id=row[0],title=row[1], description=row[2], text=row[3]) for row in a]
-    return render_template('post.html',entries=entries)
-
+    return render_template('about.html',Name=name)
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html',Name=name)
 
-@app.route('/posts/<id>')
+@app.route('/posts/<int:id>')
 def singlePost(id):
-    cur = g.db.execute("select title, description, text from entries where id = %s" %id)
+    cur = g.db.execute("select id,title, description, text, date from entries where id = %s" %id)
     a = cur.fetchall()
-    entries = [dict(title=row[0], description=row[1], text=row[2]) for row in a]
-    return render_template('post.html', entries=entries)
+    entries = [dict(id=row[0],title=row[1], description=row[2], text=row[3],d=row[4]) for row in a]
+    id=str(id)
+    return render_template('post.html',entries=entries,Name=name)
 
 @app.route('/logout')
 def logout():
@@ -107,4 +140,4 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=80)
