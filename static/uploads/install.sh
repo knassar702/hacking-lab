2d4a6be98dfc0c6ca9225d9871c40bb045d9c69a
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2019-09-13 20:06:36 +0200

    Delete install.sh

--- static/uploads/install.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/tomnomnom/httprobe
-#apt install golang
-cd httprobe
-echo 'google.com' > domains
-cat domains | go run main.go | tee lol
-#rm install.sh
-# By : knassar702
